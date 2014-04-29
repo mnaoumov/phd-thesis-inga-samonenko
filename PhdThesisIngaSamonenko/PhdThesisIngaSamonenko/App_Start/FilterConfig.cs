e360cd862f14644e0cb790e5c7f5664c135599a9
Michael Naumov <michael.naumov@fiserv.com>
2014-04-29 20:52:17 +1000

    Add MVC

---
+++ PhdThesisIngaSamonenko/PhdThesisIngaSamonenko/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace PhdThesisIngaSamonenko
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
